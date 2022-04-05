da4bf486231e4b487f653183bba8e42b93517bf4
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2022-04-04 23:06:21 -0400

    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from "vite";
+import {defineConfig, splitVendorChunkPlugin} from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [splitVendorChunkPlugin()],
   build: {
     lib: {
       entry: "src/my-element.ts",
